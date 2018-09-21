b30dab8c1389d94997f4e2ee546a1813b1a52c1e
ThinTiger <huxiaoerlai@gmail.com>
2018-09-21 15:04:18 +0800

    完全二叉树

--- src/main/java/com/konghulu/interview/domain/TreeNode.java
+++ src/main/java/com/konghulu/interview/domain/TreeNode.java
@@ -1,5 +1,6 @@
 package com.konghulu.interview.domain;
 
+import com.google.common.collect.Lists;
 import lombok.Data;
 
 import java.util.List;
@@ -14,14 +15,58 @@ public class TreeNode {
     private Integer value;
 
     /**
-     * 根据传入参数初始化一棵二叉树
+     * 根据传入参数初始化一棵完全二叉树
+     *
      * @param nodeValues 二叉树节点
      */
-    public TreeNode(List<Integer> nodeValues){
+    public TreeNode(List<Integer> nodeValues) {
+        //初始化临时队列
+        List<TreeNode> tempList = Lists.newArrayList();
+        for (int i = 0; i < nodeValues.size(); i++) {
+            TreeNode treeNode = null;
+            if (i == 0) {
+                //初始化当前对象为根节点
+                this.value = nodeValues.get(i);
+                treeNode = this;
+            } else {
+                treeNode = new TreeNode(nodeValues.get(i), null, null);
+            }
+            tempList.add(treeNode);
+        }
 
+        boolean lastChildNodeSingle = true;
+        int endIndex = tempList.size() / 2 - 1;
+        if (tempList.size() % 2 == 1) {
+            lastChildNodeSingle = false;
+        }
+        //从队列构造二叉树
+        for (int i = 0; i <= endIndex; i++) {
+            TreeNode tempNode = tempList.get(i);
+            tempNode.setLeftTreeNode(tempList.get(2 * i + 1));
+            if (i == endIndex && lastChildNodeSingle) {
+                break;
+            }
+            tempNode.setRightTreeNode(tempList.get(2 * i + 2));
+        }
     }
 
-    public TreeNode(){
+    /**
+     * 初始化一个树节点
+     *
+     * @param value
+     */
+    private TreeNode(Integer value, TreeNode leftTreeNode, TreeNode rightTreeNode) {
+        this.value = value;
+        this.leftTreeNode = leftTreeNode;
+        this.rightTreeNode = rightTreeNode;
+    }
 
+    public static void DLR(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(node.value);
+        DLR(node.leftTreeNode);
+        DLR(node.rightTreeNode);
     }
 }
--- src/test/java/konghulu/interview/konghuluinterview/KonghuluInterviewApplicationTests.java
+++ src/test/java/konghulu/interview/konghuluinterview/KonghuluInterviewApplicationTests.java
@@ -1,7 +1,9 @@
 package konghulu.interview.konghuluinterview;
 
 import com.konghulu.interview.annotation.AliasForTest;
+import com.konghulu.interview.domain.TreeNode;
 import com.konghulu.interview.server.KonghuluInterviewApplication;
+import org.assertj.core.util.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -32,5 +34,14 @@ public class KonghuluInterviewApplicationTests {
         AliasForTest annotation = AnnotationUtils.findAnnotation(getClass(), AliasForTest.class);
         System.out.println(annotation.value());
         System.out.println(annotation.alias());
+    }
+
+    /**
+     * 完全二叉树
+     */
+    @Test
+    public void fullBinaryTree() {
+        TreeNode tree = new TreeNode(Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11));
+        TreeNode.DLR(tree);
     }
 }
